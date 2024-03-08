6995bb0020a640aa89f1958a9614b4dc61e2b2b5
Carlos Elicabe <carloselicabe@bfaindustries.com>
2024-03-08 10:33:53 -0300

    Testing with php 3

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SQL Injection test 2</title>
+	<title>SQL Injection test 3</title>
 	<link rel="shortcut icon" href="../Resources/hmbct.png" />
 </head>
 <body>
